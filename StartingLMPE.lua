58ed7f4624f1fce92644f73277cc4096b74c70f6
latchmod-cell <latchmod@gmail.com>
2025-11-22 21:22:28 +0800

    Update StartingLMPE.lua

--- StartingLMPE.lua
+++ StartingLMPE.lua
@@ -12,7 +12,7 @@ print("反挂机开启")
 		   wait(1)
 		   vu:Button2Up(Vector2.new(0,0),workspace.CurrentCamera.CFrame)
 		end)
-local OrionLib = loadstring(game:HttpGet("https://sirius.menu/rayfield"))()
+local OrionLib = loadstring(game:HttpGet("https://hub.gitmirror.com/https://raw.githubusercontent.com/SiriusSoftwareLtd/Rayfield/main/source.lua"))()
 local Window = OrionLib:MakeWindow({Name = "LMPE脚本", HidePremium = false, SaveConfig = true,IntroText = "欢迎使用LMPE脚本", ConfigFolder = "欢迎LMPE脚本"})
 local about = Window:MakeTab({
     Name = "脚本名称",
